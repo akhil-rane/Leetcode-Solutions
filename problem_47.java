c639128d849d5ad074925f3080a1f86469e40377
Akhil Rane <akhil131192@gmail.com>
2017-12-16 16:39:16 -0500

    Updating solution of problem 47

--- problem_47.java
+++ problem_47.java
@@ -1,19 +1,19 @@
 class Solution {
 	public void rotate(int[][] matrix) {
 
-		for (int i = 0; i < matrix.length-i; i++) {
-			for (int j = i; j < matrix.length-i-1; j++) {
-				int temp1 = matrix[j][matrix.length-1-i];
-				matrix[j][matrix.length-1-i]=matrix[i][j];
-
-				int temp2 = matrix[matrix.length-1-i][matrix.length-1-j];
-				matrix[matrix.length-1-i][matrix.length-1-j] = temp1;
-
-				int temp3 = matrix[matrix.length-1-j][i]; 
-				matrix[matrix.length-1-j][i] = temp2;
-				
-				matrix[i][j] = temp3;
-                
+		for (int i = 0; i < matrix.length; i++) {
+			for (int j = i; j < matrix.length; j++) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[j][i];
+				matrix[j][i] = temp;
+			}
+		}
+        
+        for (int i = 0; i < matrix.length; i++) {
+			for (int j = 0; j < matrix.length/2; j++) {
+				int temp = matrix[i][j];
+				matrix[i][j] = matrix[i][matrix.length-1-j];
+				matrix[i][matrix.length-1-j] = temp;
 			}
 		}
 	}
